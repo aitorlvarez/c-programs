dcd3125b1019adf2c9489ba09184e51ce169092c
aitorlvarez <aitorlvarez@gmail.com>
2016-06-02 12:28:04 +0200

    Calculadora fracciones continuas de 4 números

---
+++ Fracc_Continuas/fracc_continuas.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+
+	
+	int i = 0;
+	float valores[4];
+	int contador = 0;
+	float resul = 0;
+
+	while (contador != 4) {
+		printf("Introduce el valor de los numeros a sumar: ");
+		scanf("%f", &valores[i]);
+		contador++;
+		i++;
+	}
+
+	
+
+	printf("\nHas introducido %i numeros\n", contador);
+	for (int i = contador; i > 0; i--) {
+		printf("\nEl valor de N es: %f\n", valores[i-1]);	
+	}
+	for (int k = contador-1; k > 0; k--) {
+		resul = valores[k - 1] + 1 / (valores[k]);
+	printf("\nOperacion%i%:\t%f\n\n", k, resul);
+		
+	}
+	
+	
+	
+	
+    return 0;
+}
+
